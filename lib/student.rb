ecff5caa0503edf9b2c31bde7fe1d20af543248a
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:20:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,11 +30,15 @@ class Student
     end
     
     def save
+      if self.id
+        self.update
+      else
       sql = <<-SQL
         INSERT INTO students (name, grade) VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0][0]
+    end
     end
     
     def self.create (name:, grade:)
