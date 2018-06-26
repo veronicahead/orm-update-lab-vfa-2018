bcf703620a89d678acfa32369a92c03966d2e48c
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:21:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0][0]
-    end
+    
     end
     
     def self.create (name:, grade:)
