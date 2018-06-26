341525deff894e34cb4c5fdd055138171ba680ca
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:38:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,9 +64,9 @@ class Student
       end.first
     end
     
-    def self.update
-      sql = "UPDATE students SET name=?, grade=? WHERE id=?"
+    def update
+      sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
       DB[:conn].execute(sql, self.name, self.grade, self.id)
-      end
+    end
       
 end
