8c1fb839f00e78f89779a0c80d66892b8ee47f8b
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:33:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-    def initialize (name, grade, id=nil)
+    def initialize (name, grade, id)
       @name = name
       @grade = grade
       @id = id
