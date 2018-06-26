7f50b91d9e5a3326c2dead9f6bcc9d828e70b8a2
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:37:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,12 +47,10 @@ class Student
     end
     
     def self.new_from_db(row)
-      # create a new Student object given a row from the database
-      new_student = self.new
-      new_student.id = row[0]
-      new_student.name = row[1]
-      new_student.grade = row[2]
-      new_student
+      id = row[0]
+      name = row[1]
+      grade = row[2]
+      self.new(id, name, grade)
     end
     
     def self.find_by_name(name)
