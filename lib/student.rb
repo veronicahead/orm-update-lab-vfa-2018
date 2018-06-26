d123b48fd6699f4742c7839f075f2d8eb52fbeee
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:24:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,4 +55,15 @@ class Student
       new_student.grade = row[2]
       new_student
     end
+    
+    def self.find_by_name(name)
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE name = ?
+        LIMIT 1
+      SQL
+      DB[:conn].execute(sql,name).map do |row| self.new_from_db(row)
+      end.first
+    end
 end
